a5d047d3844a798e1f2058d321978571006ddefe
Mateus-Magarinus <mateusmagarinus@gmail.com>
2020-03-19 17:19:37 -0300

    pasta receita adicionada

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ReceitaModule } from './receita/receita.module';
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [ReceitaModule],
 })
 export class AppModule {}
---
+++ src/receita/receita.spec.ts
@@ -0,0 +1,7 @@
+import { Receita } from './receita';
+
+describe('Receita', () => {
+  it('should be defined', () => {
+    expect(new Receita()).toBeDefined();
+  });
+});
